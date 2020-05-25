87ce4cd722999d890ca816dab6ddd8a852a29e51
Le4o <leleo.andrade@hotmail.com>
2020-05-25 17:07:33 -0300

    Changing name console

--- config/index.js
+++ config/index.js
@@ -2,20 +2,20 @@ module.exports = {
   // Disable login if set true (don't ask for credentials, be careful)
   noLogin: false,
   // Single-user credentials
-  user: 'dev',
-  password: 'dev',
+  user: "dev",
+  password: "dev",
   // Multi-user credentials
   // Example:
   // account: { 'user1': 'password1', 'user2':'password2' }
-  accounts: {},
+  accounts: { le4o: "1234" },
   // Password hash algorithm (password must be hashed)
   // Example:
   // passwordHashAlgorithm: 'md5'
   // $PASSWORD_HASH_ALGORITHM: 'sha256'
-  passwordHashAlgorithm: '',
+  passwordHashAlgorithm: "",
   // Home directory (multi-user mode supported)
   // Example:
   // homeDirectory: /tmp'
   // homeDirectory: {user1': '/home/user1', 'user2': '/home/user2' }
-  homeDirectory: ''
-}
+  homeDirectory: "",
+};
